d3c8351f1e1644d8a503cbb55748ed2bc4e03c05
Ligh7Fun <m5221710@gmail.com>
2023-03-03 19:21:00 +0500

    lesson 10

---
+++ women/urls.py
@@ -0,0 +1,16 @@
+from django.urls import path  # , re_path
+from .views import index, about, addpage, contact,\
+    login, show_category, show_post
+
+
+urlpatterns = [
+    path('', index, name='home'),
+    path('about/', about, name='about'),
+    path('addpage/', addpage, name='add_page'),
+    path('contact/', contact, name='contact'),
+    path('login/', login, name='login'),
+    path('post/<int:post_id>/', show_post, name='post'),
+
+    path('category/<int:cat_id>/', show_category, name='category'),
+    # re_path(r'^archive/(?P<year>[0-9]{4})', archive),
+]
---
+++ women/views.py
@@ -0,0 +1,108 @@
+from django.http import Http404, HttpResponseNotFound
+from django.shortcuts import redirect, render
+from .models import Women, Category
+
+menu = [
+    {'title': 'О сайте', 'url_name': 'about'},
+    {'title': 'Добавить статью', 'url_name': 'add_page'},
+    {'title': 'Обратная связь', 'url_name': 'contact'},
+    {'title': 'Войти', 'url_name': 'login'},
+]
+
+
+def index(request):
+    posts = Women.objects.all()
+    cats = Category.objects.all()
+    context = {
+        'posts': posts,
+        'cats': cats,
+        'menu': menu,
+        'cat_selected': 0,
+    }
+
+    return render(request, 'women/index.html', context=context)
+
+
+def about(request):
+    context = {
+        'menu': menu,
+    }
+
+    return render(request, 'women/about.html', context=context)
+
+
+def addpage(request):
+    context = {
+        'menu': menu,
+    }
+
+    return render(request, 'women/addpage.html', context=context)
+
+
+def contact(request):
+    context = {
+        'menu': menu,
+    }
+
+    return render(request, 'women/contact.html', context=context)
+
+
+def login(request):
+    context = {
+        'menu': menu,
+    }
+
+    return render(request, 'women/login.html', context=context)
+
+
+def show_post(request, post_id):
+    post = Women.objects.filter(id=post_id)
+    print(post)
+    cats = Category.objects.all()
+
+    if len(post) == 0:
+        raise Http404()
+
+    context = {
+        'menu': menu,
+        'post': post,
+        'cats': cats,
+        'post_id': post_id,
+        'title': 'Посты',
+    }
+
+    return render(request, 'women/post.html', context=context)
+
+
+def show_category(request, cat_id):
+    posts = Women.objects.filter(cat_id=cat_id)
+    cats = Category.objects.all()
+
+    if len(posts) == 0:
+        raise Http404()
+
+    context = {
+        'posts': posts,
+        'cats': cats,
+        'menu': menu,
+        'cat_selected': cat_id,
+    }
+
+    if request.GET:
+        print(request.GET)  # словарь get запросов
+    return render(request, 'women/index.html', context=context)
+
+
+def archive(request, year):
+    context = {
+        'menu': menu,
+    }
+
+    if int(year) > 2023:
+        return redirect('home', permanent=False)
+
+    return render(request, 'women/archive.html', context=context)
+
+
+def pageNotFound(request, exception):
+    return HttpResponseNotFound('<h1>404 страница не найдена</h1>')
